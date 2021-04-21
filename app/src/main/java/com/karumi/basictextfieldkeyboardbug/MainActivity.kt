2135a8a164a532945063d66773f74e3dae26500c
Pedro Gómez <pedro@karumi.com>
2021-04-21 09:04:22 +0200

    Try using text field value instead of plain string

--- app/src/main/java/com/karumi/basictextfieldkeyboardbug/MainActivity.kt
+++ app/src/main/java/com/karumi/basictextfieldkeyboardbug/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.karumi.basictextfieldkeyboardbug.ui.theme.BasicTextFieldKeyboardBugTheme
 
@@ -17,11 +20,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var text: String by remember { mutableStateOf("") }
+            var text by remember { mutableStateOf(TextFieldValue("")) }
             BasicTextFieldKeyboardBugTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    BasicTextField(
-                        modifier = Modifier.fillMaxSize().padding(20.dp),
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp),
                         value = text,
                         onValueChange = { text = it }
                     )
